1235fa22e7ad9794adfb5c2460e3ec458bbeb63e
xo <654720416@qq.com>
2022-02-17 16:06:29 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,99 +1,90 @@
 module.exports = {
   title: "XO'S BLOG",
-  description: '',
-  base: '/xo-blog/',
+  description: "",
+  base: "/xo-blog/",
   port: 8787,
-  head: [
-    ['link', { rel: 'icon', href: '/logo.svg' }]
-  ],
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
   themeConfig: {
     nav: [
-      { text: 'Github', link: 'https://github.com/Hellottxo' },
-      { text: 'XO-UI', link: 'http://xo.silenttt.top/xo-ui' },
-      { text: 'leetcode-records', link: 'http://xo.silenttt.top/leetcode-records' }
+      { text: "Github", link: "https://github.com/Hellottxo" },
+      { text: "XO-UI", link: "http://xo.silenttt.top/xo-ui" },
+      {
+        text: "leetcode-records",
+        link: "http://xo.silenttt.top/leetcode-records",
+      },
     ],
     sidebar: [
       {
-        title: 'js',
-        key: 'js',
+        title: "js",
+        key: "js",
         collapsable: false,
         sidebarDepth: 3,
         children: [
-          '/js/this',
-          '/js/variable',
-          '/js/prototype',
-          '/js/inheritance',
-          '/js/handleWrite',
-          '/js/promise',
-          '/js/implicitConversion',
-          '/js/event',
-          '/js/numInJs'
-        ]
-      },
-      {
-        title: 'css',
-        key: 'css',
-        collapsable: false,
-        sidebarDepth: 3,
-        children: [
-          '/css/transform',
-          '/css/loading-animation',
-          '/css/css'
+          "/js/this",
+          "/js/variable",
+          "/js/prototype",
+          "/js/inheritance",
+          "/js/handleWrite",
+          "/js/promise",
+          "/js/implicitConversion",
+          "/js/event",
+          "/js/numInJs",
+          "/js/lottie-web",
         ],
       },
       {
-        title: 'html',
-        key: 'html',
+        title: "css",
+        key: "css",
         collapsable: false,
         sidebarDepth: 3,
         children: [
-          '/html/h5',
+          "/css/transform",
+          "/css/loading-animation",
+          "/css/css",
+          "/css/response",
         ],
       },
       {
-        title: 'Vue',
-        key: 'Vue',
+        title: "html",
+        key: "html",
         collapsable: false,
         sidebarDepth: 3,
-        children: [
-          '/Vue/frame',
-          '/Vue/lifecycle',
-          '/Vue/computed',
-          '/Vue/desginPattern'
-        ]
+        children: ["/html/h5"],
       },
       {
-        title: '浏览器',
-        key: 'browser',
+        title: "Vue",
+        key: "Vue",
         collapsable: false,
         sidebarDepth: 3,
         children: [
-          '/browser/eventloop',
-          '/browser/cache',
-          '/browser/reflow'
-        ]
+          "/Vue/frame",
+          "/Vue/lifecycle",
+          "/Vue/computed",
+          "/Vue/desginPattern",
+        ],
       },
       {
-        title: '计算机基础',
-        key: 'computer',
+        title: "浏览器",
+        key: "browser",
         collapsable: false,
         sidebarDepth: 3,
-        children: [
-          '/computer/http',
-          '/computer/howNetworkConnect'
-        ]
+        children: ["/browser/eventloop", "/browser/cache", "/browser/reflow"],
       },
       {
-        title: '算法',
-        key: 'algorithm',
+        title: "计算机基础",
+        key: "computer",
         collapsable: false,
         sidebarDepth: 3,
-        children: [
-          '/algorithm/sort',
-          '/algorithm/subject'
-        ]
-      }
+        children: ["/computer/http", "/computer/howNetworkConnect"],
+      },
+      {
+        title: "算法",
+        key: "algorithm",
+        collapsable: false,
+        sidebarDepth: 3,
+        children: ["/algorithm/sort", "/algorithm/subject"],
+      },
     ],
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
   },
 };
